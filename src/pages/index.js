c717d698206806d8a29d470b9433eb87d48cdc7f
Rocky Kev <kev.rocky@gmail.com>
2018-12-10 21:32:19 -0800

    first

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default () => (
+  <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
+)
+
+/* 
+Powershell -> gatsby develop
+
+https://www.gatsbyjs.org/tutorial/part-zero/#create-a-gatsby-site
+
+You’ll be able to visit the site locally at http://localhost:8000 for as long as your development server is running. That’s the process you started by running the gatsby develop command. To stop running that process (or to “stop running the development server”), go back to your terminal window, hold down the “control” key, and then hit “c” (ctrl-c). To start it again, run gatsby develop again!
+
+*/
