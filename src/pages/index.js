4ae45dc2259da1cd531eb67270b1197d0d33b9da
Rocky Kev <6101650+RockyKev@users.noreply.github.com>
2018-12-22 15:28:49 -0800

    changed header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 export default () => (
-  <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
+  <div style={{ color: `purple`, fontSize: `72px` }}>Hello you Gatsby!</div>
 )
 
 /* 
